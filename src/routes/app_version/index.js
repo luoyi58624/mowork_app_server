b98a08b2091f17a6f4f269e76b40e18257daa4b0
luoyi <luoyi58624@gmail.com>
2023-07-17 08:31:16 +0800

    修复编辑app版本错误

--- src/routes/app_version/index.js
+++ src/routes/app_version/index.js
@@ -1,143 +1,111 @@
 var express = require('express')
 const model = require('./model')
 const multer = require('multer')
-const { check, validationResult } = require('express-validator')
-const { isEmpty } = require('../../utils/common')
+const {check, validationResult} = require('express-validator')
+const {isEmpty} = require('../../utils/common')
 
 var router = express.Router()
 
 const storage = multer.diskStorage({
-	destination(req, file, callback) {
-		callback(null, './public/apk')
-	},
-	filename(req, file, callback) {
-		callback(null, file.originalname)
-	}
+  destination(req, file, callback) {
+    callback(null, './public/apk')
+  },
+  filename(req, file, callback) {
+    callback(null, file.originalname)
+  }
 })
 
 const upload = multer({
-	storage,
-	fileFilter: (req, file, callback) => {
-		if (file.originalname.endsWith('.apk')) {
-			file.originalname = Buffer.from(file.originalname, 'latin1').toString('utf8')
-			callback(null, true)
-		} else {
-			callback(new Error('上传的文件类型必须为apk'))
-		}
-	}
+  storage,
+  fileFilter: (req, file, callback) => {
+    if (file.originalname.endsWith('.apk')) {
+      file.originalname = Buffer.from(file.originalname, 'latin1').toString('utf8')
+      callback(null, true)
+    } else {
+      callback(new Error('上传的文件类型必须为apk'))
+    }
+  }
 })
 
 router.get('/', async (req, res, next) => {
-	res.send({
-		code: 200,
-		data: await model.find().sort({ versionNum: -1 }).exec()
-	})
+  res.send({
+    code: 200,
+    data: await model.find().sort({versionNum: -1}).exec()
+  })
 })
 
 router.post(
-	'/',
-	upload.single('file'),
-	[check('versionName', '请传入版本名字').notEmpty(), check('versionNum', '请传入版本号').notEmpty()],
-	async (req, res, next) => {
-		if (isEmpty(req.file)) {
-			return res.send({
-				code: 500,
-				msg: '请传入文件'
-			})
-		}
-		const result = validationResult(req)
-		if (!result.isEmpty()) {
-			return res.send({
-				code: 500,
-				msg: result.array()
-			})
-		}
-		await model({
-			...req.body,
-			appName: req.file.originalname,
-			fileSize: req.file.size,
-			downloadUrl: `/apk/${req.file.originalname}`
-		}).save()
-		res.send({
-			code: 200,
-			data: '上传成功'
-		})
-	}
+  '/',
+  upload.single('file'),
+  [check('versionName', '请传入版本名字').notEmpty(), check('versionNum', '请传入版本号').notEmpty()],
+  async (req, res, next) => {
+    console.log(req.body)
+    if (isEmpty(req.file)) {
+      return res.send({
+        code: 500,
+        msg: '请传入文件'
+      })
+    }
+    const result = validationResult(req)
+    if (!result.isEmpty()) {
+      return res.send({
+        code: 500,
+        msg: result.array()
+      })
+    }
+    await model({
+      ...req.body,
+      appName: req.file.originalname,
+      fileSize: req.file.size,
+      downloadUrl: `/apk/${req.file.originalname}`
+    }).save()
+    res.send({
+      code: 200,
+      data: '上传成功'
+    })
+  }
 )
 
 router.put(
-	'/',
-	upload.single('file'),
-	[check('_id', '请传入_id').notEmpty(), check('versionName', '请传入版本名字').notEmpty(), check('versionNum', '请传入版本号').notEmpty()],
-	async (req, res, next) => {
-		if (isEmpty(req.file)) {
-			return res.send({
-				code: 500,
-				msg: '请传入文件'
-			})
-		}
-		const result = validationResult(req)
-		if (!result.isEmpty()) {
-			return res.send({
-				code: 500,
-				msg: result.array()
-			})
-		}
-    await model({
-			...req.body,
-			appName: req.file.originalname,
-			fileSize: req.file.size,
-			downloadUrl: `/apk/${req.file.originalname}`
-		}).save()
-		// const target = await model.findOne({ _id: req.body._id })
-		// await target.updateOne({
-		// 	...req.body,
-		// 	appName: req.file.originalname,
-		// 	fileSize: req.file.size,
-		// 	downloadUrl: `/apk/${req.file.originalname}`
-		// })
-		res.send({
-			code: 200,
-			data: '更新成功'
-		})
-	}
+  '/',
+  upload.single('file'),
+  [check('_id', '请传入_id').notEmpty(), check('versionName', '请传入版本名字').notEmpty(), check('versionNum', '请传入版本号').notEmpty()],
+  async (req, res, next) => {
+    console.log(req.body)
+    const result = validationResult(req)
+    if (!result.isEmpty()) {
+      return res.send({
+        code: 500,
+        msg: result.array()
+      })
+    }
+
+    await model.findByIdAndUpdate(req.body['_id'], {
+      ...req.body,
+      appName: req.file?.originalname ?? undefined,
+      fileSize: req.file?.size ?? undefined,
+      downloadUrl: req.file?.originalname ? `/apk/${req.file.originalname}` : undefined
+    })
+    res.send({
+      code: 200,
+      data: '更新成功'
+    })
+  }
 )
 
-const editForm = async (req, res, type) => {
-	const result = validationResult(req)
-	if (!result.isEmpty()) {
-		return res.send({
-			code: 500,
-			msg: result.array()
-		})
-	}
-
-	const data = {
-		...req.body,
-		appName: req.file.originalname,
-		fileSize: req.file.size,
-		downloadUrl: `/apk/${req.file.originalname}`
-	}
-	type == 0 ? await model(data).save() : await model(data)
-	res.send({
-		code: 200,
-		data: '上传成功'
-	})
-}
-
 router.delete('/:id', async (req, res, next) => {
-	await model.deleteMany({ _id: { $in: req.params.id.split(',') } })
-	res.send({
-		code: 200,
-		data: '删除成功'
-	})
+  await model.deleteMany({_id: {$in: req.params.id.split(',')}})
+  res.send({
+    code: 200,
+    data: '删除成功'
+  })
 })
 
 router.get('/newVersion', async (req, res, next) => {
-	res.send({
-		code: 200,
-		data: await model.find().sort({ versionNum: -1 }).limit(1).exec()
-	})
+  res.send({
+    code: 200,
+    data: await model.find().sort({versionNum: -1}).limit(1).exec()
+  })
 })
 
 module.exports = router
