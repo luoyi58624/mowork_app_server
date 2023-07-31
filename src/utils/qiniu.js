77bd901eb831d8fb3c443773c7f8b8fa9f3ce91f
luoyi <luoyi58624@163.com>
2023-07-31 16:54:03 +0800

    删除app版本将移除七牛云上的文件

--- src/utils/qiniu.js
+++ src/utils/qiniu.js
@@ -1,25 +1,35 @@
-const qiniu = require('qiniu')
+const qiniu = require("qiniu");
 
-const qiniuHttp = 'http://cdn.liyi.online'
+const qiniuHttp = "http://cdn.liyi.online";
+
 // 七牛云认证
-const accessKey = 'hdUy8w7obc3Y8txW0zj4SCN5AazLNq6idz9rYVKR'
-const secretKey = 'ZTRsC8HloMDAo-ngwl0kCbi-sNujHQc5c94br-7P'
-const mac = new qiniu.auth.digest.Mac(accessKey, secretKey)
+const accessKey = "hdUy8w7obc3Y8txW0zj4SCN5AazLNq6idz9rYVKR";
+const secretKey = "ZTRsC8HloMDAo-ngwl0kCbi-sNujHQc5c94br-7P";
+const mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
+
+// 七牛云文件上传的空间
+const qiniuBucket = "luoyi-test";
 
 function getUploadToken(fileName) {
-	// 创建上传token
-	const putPolicy = new qiniu.rs.PutPolicy({
-		scope: 'luoyi-test:' + fileName // 空间名
-	})
-	const uploadToken = putPolicy.uploadToken(mac)
-	return uploadToken
+  // 创建上传token
+  const putPolicy = new qiniu.rs.PutPolicy({
+    scope: qiniuBucket + ":" + fileName // 空间名
+  });
+  const uploadToken = putPolicy.uploadToken(mac);
+  return uploadToken;
 }
+
 // 创建七牛云配置
-const qiniuConfig = new qiniu.conf.Config()
-qiniuConfig.zone = qiniu.zone.Zone_z2 // 机房所在地
+const qiniuConfig = new qiniu.conf.Config();
+qiniuConfig.zone = qiniu.zone.Zone_z2; // 机房所在地
+
+// 创建七牛云空间管理
+const bucketManager = new qiniu.rs.BucketManager(mac, qiniuConfig);
 
 module.exports = {
-	qiniuHttp,
-	qiniuConfig,
-	getUploadToken
-}
+  qiniuHttp,
+  qiniuBucket,
+  qiniuConfig,
+  bucketManager,
+  getUploadToken
+};
