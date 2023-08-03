23ca7840ad8c0ab19dd4c97b129a0681a9b5c3c4
luoyi <luoyi58624@163.com>
2023-08-03 18:19:05 +0800

    文件上传移除apk限制

--- src/routes/other/upload.js
+++ src/routes/other/upload.js
@@ -8,7 +8,6 @@ const storage = multer.diskStorage({
 		callback(null, './public/apk')
 	},
 	filename(req, file, callback) {
-		console.log(file)
 		callback(null, file.originalname)
 	}
 })
@@ -19,12 +18,8 @@ const upload = multer({
 		fileSize: 1024 * 1024 * 50
 	},
 	fileFilter: (req, file, callback) => {
-		if (file.originalname.endsWith('.apk')) {
-			file.originalname = Buffer.from(file.originalname, 'latin1').toString('utf8')
-			callback(null, true)
-		} else {
-			callback(new Error('上传的文件类型必须为apk'))
-		}
+		file.originalname = Buffer.from(file.originalname, 'latin1').toString('utf8')
+		callback(null, true)
 	}
 })
 
